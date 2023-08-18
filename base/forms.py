7368351adb85270e23075a42a12c06b7bd391683
Julius Charles <cjmatheka@gmail.com>
2023-08-18 18:47:42 +0300

    Creating form for building rooms

---
+++ base/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from .models import Room
+
+class RoomForm(ModelForm):
+    class meta:
+        model = Room
+        fields = '__all__'
+
+
+
+
+
+
+
+
+
+
+
+
+
